e0a84b80382350f07e2358f521e2d2482e2c70aa
Jiwo Kristi <priyadijiwo@gmail.com>
2026-01-05 14:20:29 +0800

    feat: update password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -206,5 +206,24 @@ export const resetPassword = catchAsync(
 );
 
 export const updatePassword = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    // 1) Get user from collection
+    const user = await User.findById(req.user.id).select('+password');
+
+    // 2) Check if POSTed current password is correct
+    if (
+      !user ||
+      !(await user.correctPassword(req.body.passwordCurrent, user.password))
+    ) {
+      return next(new AppError('Your current password is wrong.', 401));
+    }
+
+    // 3) If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    await user.save();
+
+    // 4) Log user in, send JWT
+    createSendToken(user, 200, res);
+  },
 );
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -14,6 +14,8 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.use(authController.protect);
 
+router.patch('/updateMyPassword', authController.updatePassword);
+
 router.use(authController.restrictTo('admin'));
 
 router.route('/').get(userController.getAllUsers);
