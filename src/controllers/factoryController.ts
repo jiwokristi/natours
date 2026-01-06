2863c2f878712105e52fc7844b83b84c3df87dc8
Jiwo Kristi <priyadijiwo@gmail.com>
2026-01-06 14:24:54 +0800

    add: apiFeatures

---
+++ src/controllers/factoryController.ts
@@ -0,0 +1,95 @@
+import { Document, Model, PopulateOptions } from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
+
+import catchAsync from 'utils/catchAsync.js';
+import AppError from 'utils/appError.js';
+import APIFeatures from 'utils/apiFeatures.js';
+
+export const deleteOne = <T extends Document>(Model: Model<T>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+export const updateOne = <T extends Document>(Model: Model<T>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+export const createOne = <T extends Document>(Model: Model<T>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+export const getOne = <T extends Document>(
+  Model: Model<T>,
+  popOptions?: PopulateOptions | PopulateOptions[],
+) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+export const getAll = <T extends Document>(Model: Model<T>) =>
+  catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // To allow for nested GET reviews on tour (hack)
+    let filter: Record<string, any> = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
