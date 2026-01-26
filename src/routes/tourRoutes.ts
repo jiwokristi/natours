9be98d5f48705f1760cf0806129b01783c5dfb1d
Jiwo Kristi <priyadijiwo@gmail.com>
2026-01-26 14:33:33 +0800

    protect tour routes

--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,18 +1,31 @@
 import express from 'express';
 
 import * as tourController from 'controllers/tourController.js';
+import * as authController from 'controllers/authController.js';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour,
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour,
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour,
+  );
 
 export default router;
